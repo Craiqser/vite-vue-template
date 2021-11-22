fb7fa68b9211c8062f0698feff6fe98092c3f59a
Sobolev Sergey <craiqser@gmail.com>
2021-11-22 14:55:38 +0300

    chore: playground container

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,25 @@
 <template>
-<span>Hi</span>
+<header />
+<main>
+	<section class="container">
+		<span>Hi</span>
+	</section>
+</main>
+<footer />
 </template>
 
 <style lang='scss'>
 @import '~/assets/base.scss';
+
 body {
 	display: grid;
 	grid-template-rows: auto 1fr auto;
+}
+
+.container {
+	height: 100%;
+	display: flex;
+	align-items: center;
+	justify-content: center;
 }
 </style>
