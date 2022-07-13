c5311706ce4dea8d5690eb25d243b8b16dfadd56
Sobolev Sergey <craiqser@gmail.com>
2022-07-13 17:04:00 +0300

    feat: Using esbuild deps optimization at build time

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,14 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
+	build: {
+		commonjsOptions: {
+			include: []
+		}
+	},
+	optimizeDeps: {
+		disabled: false
+	},
 	plugins: [vue()],
 	resolve: {
 		alias: {
